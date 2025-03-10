dd7b3a6e7de3f8bb8f7ed311148f15d01fe72dff
vinhed <vinhed@gmail.com>
2025-03-10 15:55:11 +0100

    Updated Bets

--- src/api/allsvenskanApi.js
+++ src/api/allsvenskanApi.js
@@ -4,7 +4,7 @@
 export const fetchFullData = async () => {
   try {
     try {
-      const localResponse = await fetch(`https://raw.githubusercontent.com/vinhed/grabbarnas-allsvenskan2025/refs/heads/main/public/data/standings.json`);
+      const localResponse = await fetch(`https://raw.githubusercontent.com/vinhed/grabbarnas-allsvenskan2025/refs/heads/main/public/data/standings.json?nocache=${new Date().getTime()}`);
       if (!localResponse.ok) {
         throw new Error(`Failed to fetch bets data: ${localResponse.status}`);
       }
